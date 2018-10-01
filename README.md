22711e3317f34cc6f15b2b3d8ad9fb1b2dbf395f
Matthew Reese <mindblown012@hotmail.com>
2018-10-01 11:28:39 -0700

    Inital Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mind Blown Bot
+<hr />
+A bot made to control the Discord server Greatness of technology.
---
+++ index.js
@@ -0,0 +1,24 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("ready", async () => {
+	console.log(`${bot.user.username} is online `);
+	bot.user.setActivity("Being created by human!!");
+})
+
+bot.on("message", async (message) => {
+	if(message.author.bot) return;
+	if(message.channel.type == "dm") return;
+
+	let prefix = botconfig.prefix;
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray[0];
+	let args = messageArray.slice(1);
+
+	if(cmd == `${prefix}hello`) {
+		return message.channel.send("Hello!");
+	}
+})
+
+bot.login(botconfig.token);
